adcd2e92f970a0299b8583423ac2e09e8f433284
larbert <17835126760@163.com>
2022-08-18 19:13:13 +0800

    分级log

--- log.go
+++ log.go
@@ -1,3 +1,50 @@
 package mayflylog
 
-const A = 1
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	errorLog = log.New(os.Stdout, "\033[31m[error]\033[0m ", log.LstdFlags|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, "\033[34m[info ]\033[0m ", log.LstdFlags|log.Lshortfile)
+	debugLog = log.New(os.Stdout, "\033[34m[debug]\033[0m ", log.LstdFlags|log.Lshortfile)
+	loggers  = []*log.Logger{errorLog, infoLog, debugLog}
+	mu       sync.Mutex
+)
+
+// log methods
+var (
+	Error  = errorLog.Println
+	Errorf = errorLog.Printf
+	Info   = infoLog.Println
+	Infof  = infoLog.Printf
+	Debug  = debugLog.Println
+	Debugf = debugLog.Printf
+)
+
+// log levels
+const (
+	DebugLevel = iota
+	InfoLevel
+	ErrorLevel
+)
+
+func SetLevel(level int) {
+	mu.Lock()
+	defer mu.Unlock()
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+	if level > InfoLevel {
+		debugLog.SetOutput(ioutil.Discard)
+	}
+	if level > InfoLevel {
+		infoLog.SetOutput(ioutil.Discard)
+	}
+	if level > ErrorLevel {
+		errorLog.SetOutput(ioutil.Discard)
+	}
+}
